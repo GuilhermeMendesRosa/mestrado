6fc215a48b47035510de7889c323d865819dca89
Guilherme Mendes Rosa <guilhermemr.jlle@gmail.com>
2025-07-14 22:35:43 -0300

    .

---
+++ lbot-datagen/lbot-datagen-backend/src/main/java/br/com/roselabs/lbot_datagen_backend/LbotDatagenBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.roselabs.lbot_datagen_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbotDatagenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbotDatagenBackendApplication.class, args);
+	}
+
+}
---
+++ lbot-datagen/lbot-datagen-backend/src/test/java/br/com/roselabs/lbot_datagen_backend/LbotDatagenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.roselabs.lbot_datagen_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbotDatagenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
